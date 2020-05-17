f3a679b5ace6c8a12f89237a632e61acf8e0dbea
Tomoya Sawada <blackie0110@gmail.com>
2020-05-17 18:35:19 +0900

    [UPDATE] run gan

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from general import *
+from GAN import main as gan_main
 
-def main()
+def main():
+    image_size = 100
+    batch_size = 64
+
+    dataset = AnimeFaceDataset(image_size=image_size)
+    dataset = to_loader(dataset=dataset, batch_size=batch_size)
+
+    gan_main(
+        dataset=dataset,
+        image_size=image_size
+    )
 
 if __name__ == "__main__":
     main()
