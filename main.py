b0574836d6543b2893e67fdb4232a768bdb51fde
Tomoya Sawada <blackie0110@gmail.com>
2020-05-18 10:46:40 +0900

    [UPDATE] train dcgan

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from general import *
 from GAN import main as gan_main
+from DCGAN import main as dcgan_main
 
 def main():
-    image_size = 100
+    image_size = 128
     batch_size = 64
 
     dataset = AnimeFaceDataset(image_size=image_size)
     dataset = to_loader(dataset=dataset, batch_size=batch_size)
 
-    gan_main(
+    dcgan_main(
         dataset=dataset,
         image_size=image_size
     )
