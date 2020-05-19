9660e8553ad3362a1dcd39716870807252403de4
Tomoya Sawada <blackie0110@gmail.com>
2020-05-19 15:18:52 +0900

    [UPDATE] train cGAN

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from general import *
 from GAN import main as gan_main
 from DCGAN import main as dcgan_main
+from cGAN import main as cgan_main
 
 def main():
     image_size = 128
     batch_size = 64
 
-    dataset = AnimeFaceDataset(image_size=image_size)
+    # dataset = AnimeFaceDataset(image_size=image_size)
+    dataset = OneHotLabeledAnimeFaceDataset(image_size=image_size)
     dataset = to_loader(dataset=dataset, batch_size=batch_size)
 
-    dcgan_main(
+    cgan_main(
         dataset=dataset,
         image_size=image_size
     )
