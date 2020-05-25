180d02ad6c233c5e70ff22d02a54e92099460826
Tomoya Sawada <blackie0110@gmail.com>
2020-05-25 12:36:53 +0900

    [UPDATE] add danbooru dataset

--- general/__init__.py
+++ general/__init__.py
@@ -1,3 +1,11 @@
 
-from .loader import AnimeFaceDataset, LabeledAnimeFaceDataset, OneHotLabeledAnimeFaceDataset
-from .loader import to_loader
+from .anime_face import AnimeFaceDataset, LabeledAnimeFaceDataset, OneHotLabeledAnimeFaceDataset
+from .danbooru import DanbooruDataset, GeneratePairImageDanbooruDataset
+
+def to_loader(
+    dataset,
+    batch_size,
+    shuffle=True
+):
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+    return loader
