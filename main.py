f4a31dada2a9e12112ce510c5e337392fed21470
Tomoya Sawada <blackie0110@gmail.com>
2020-06-28 10:07:51 +0900

    [UPDATE] train pggan

--- main.py
+++ main.py
@@ -7,25 +7,32 @@ from pixelshuffle import main as pixelshuffle_main
 from WGAN import main as wgan_main
 from WGAN_gp import main as wgan_gp_main
 
+from PGGAN import main as pggan_main
+
 from pix2pix import main as pix2pix_main
 
 def main():
-    image_size = 128
-    batch_size = 64
+    # image_size = 128
+    # batch_size = 64
 
-    dataset = AnimeFaceDataset(image_size=image_size)
+    # dataset = AnimeFaceDataset(image_size=image_size)
     # dataset = OneHotLabeledAnimeFaceDataset(image_size=image_size)
-    dataset = to_loader(dataset=dataset, batch_size=batch_size)
+    # dataset = to_loader(dataset=dataset, batch_size=batch_size)
 
-    wgan_gp_main(
-        dataset=dataset,
-        image_size=image_size
-    )
+    # wgan_gp_main(
+    #     dataset=dataset,
+    #     image_size=image_size
+    # )
 
     # pix2pix_main(
     #     GeneratePairImageDanbooruDataset,
     #     to_loader
     # )
+
+    pggan_main(
+        AnimeFaceDataset,
+        to_loader
+    )
 
 if __name__ == "__main__":
     main()
