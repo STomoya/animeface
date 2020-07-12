261200bc053648a7cf7fadd35a7d547d8da563d2
Tomoya Sawada <blackie0110@gmail.com>
2020-07-12 10:27:47 +0900

    [ADD] add num_worker option

--- general/__init__.py
+++ general/__init__.py
@@ -9,7 +9,8 @@ from torch.utils.data import DataLoader
 def to_loader(
     dataset,
     batch_size,
-    shuffle=True
+    shuffle=True,
+    num_workers=8
 ):
-    loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
     return loader
