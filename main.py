34ee565f020d9b146dcc6ad68afcae793a885397
Tomoya Sawada <blackie0110@gmail.com>
2020-07-26 09:45:29 +0900

    [UPDATE] train on stylegan2

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from WGAN_gp import main as wgan_gp_main
 
 from PGGAN import main as pggan_main
 from StyleGAN import main as stylegan_main
+from StyleGAN2 import main as stylegan2_main
 
 from DiffAugment import main as da_main
 
@@ -28,15 +29,16 @@ def main():
     #     image_size=image_size
     # )
 
-    ugatit_main(
-        GeneratePairImageDanbooruDataset,
-        to_loader
-    )
-
-    # da_main(
-    #     AnimeFaceDataset,
+    # ugatit_main(
+    #     GeneratePairImageDanbooruDataset,
     #     to_loader
     # )
+
+    stylegan2_main(
+        AnimeFaceDataset,
+        to_loader,
+        DiffAugment
+    )
 
 if __name__ == "__main__":
     main()
