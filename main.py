a79256d29392a70f2490a58c7ec069e253684138
Tomoya Sawada <blackie0110@gmail.com>
2020-09-17 11:42:12 +0900

    [UPDATE] call main without args

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from implementations.general import *
+
 from implementations.GAN import main as gan_main
 from implementations.DCGAN import main as dcgan_main
 from implementations.cGAN import main as cgan_main
@@ -20,30 +20,7 @@ from implementations.SinGAN import main as singan_main
 from implementations.DiffAugment import main as da_main
 
 def main():
-    # image_size = 128
-    # batch_size = 64
-
-    # dataset = AnimeFaceDataset(image_size=image_size)
-    # dataset = OneHotLabeledAnimeFaceDataset(image_size=image_size)
-    # dataset = to_loader(dataset=dataset, batch_size=batch_size)
-
-    # wgan_gp_main(
-    #     dataset=dataset,
-    #     image_size=image_size
-    # )
-
-    # ugatit_main(
-    #     GeneratePairImageDanbooruDataset,
-    #     to_loader
-    # )
-
-    hologan_main(
-        AnimeFaceDataset,
-        to_loader,
-        DiffAugment
-    )
-
-    # singan_main()
+    hologan_main()
 
 if __name__ == "__main__":
     main()
