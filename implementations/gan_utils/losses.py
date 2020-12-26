bdf566f6e7aed4e5e9f0ce35f414c54bfee45ae4
STomoya <blackie0110@gmail.com>
2020-12-26 21:03:08 +0900

    [UPDATE] grad penalty with amp support

--- implementations/gan_utils/losses.py
+++ implementations/gan_utils/losses.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import grad, Variable
+from torch.cuda.amp import autocast, GradScaler
 
 
 '''
@@ -152,5 +153,123 @@ class HingeLoss(AdversarialLoss):
 Regularization
 '''
 
+class GradPenalty(Loss):
+    def _calc_grad(self, outputs, inputs, scaler=None):
+        '''calculate gradients
 
+        args
+            outputs: torch.Tensor
+                output tensor of a model
+            inputs: torch.Tensor
+                input tensor to a model
+            scaler: torch.cuda.amp.GradScaler (default: None)
+                gradient scaler if using torch.cuda.amp
+        '''
+        with autocast(False):
+            if isinstance(scaler, GradScaler):
+                outputs = scaler.scale(outputs)
+            ones = torch.ones(outputs.size(), device=outputs.device)
+            gradients = grad(
+                outputs=outputs,
+                inputs=inputs,
+                grad_outputs=ones,
+                create_graph=True,
+                retain_graph=True,
+                only_inputs=True
+            )[0]
+            if isinstance(scaler, GradScaler):
+                gradients = gradients / scaler.get_scale()
+        return gradients
 
+    def gradient_penalty(self, real, fake, D, scaler=None, center=1.):
+        '''calculate gradient penalty (1 and 0 center)
+
+        args
+            real: torch.Tensor
+                batched tensor of real images
+            fake: torch.Tensor
+                generated image tensor
+            D: torch.nn.Module
+                discriminator to calculate the gradient for
+            scaler: torch.cuda.amp.GradScaler (default: None)
+                gradient scaler for torch.cuda.amp
+            center: Union[int, float] (default: 1.)
+                the center used the calculate the penalty
+        '''
+        assert center in [1., 0., 1, 0]
+
+        device = real.device
+
+        alpha = torch.rand(1, device=device)
+        x_hat = real * alpha + fake * (1 - alpha)
+        x_hat = Variable(x_hat, requires_grad=True)
+
+        d_x_hat = D(x_hat)
+        
+        gradients = _calc_grad(d_x_hat, x_hat, scaler)
+
+        gradients = gradients.reshape(gradients.size(0), -1)
+        penalty = (gradients.norm(2, dim=1) - center).pow(2).mean()
+
+        if self.backward:
+            penalty.backward(retain_graph=True)
+
+        return penalty
+
+    def dragan_gradient_penalty(self, real, D, scaler=None):
+        '''DRAGAN gradient penalty
+
+        args:
+            real: torch.Tensor
+                batched tensor of real images
+            D: torch.nn.Module
+                discriminator to calculate the gradient for
+            scaler: torch.cuda.amp.GradScaler (default: None)
+                gradient scaler for torch.cuda.amp
+        '''
+
+        device = real.device
+
+        alpha = torch.rand((real.size(0), 1), device=device)
+        beta = torch.rand(real.size(), device=device)
+        x_hat = real * alpha + (1 - alpha) * (real + 0.5 * real.std() * beta)
+        x_hat = Variable(x_hat, requires_grad=True)
+
+        d_x_hat = D(x_hat)
+
+        gradients = _calc_grad(d_x_hat, x_hat, scaler)
+
+        gradients = gradients.reshape(gradients.size(0), -1)
+        penalty = (gradients.norm(2, dim=1) - center).pow(2).mean()
+
+        if self.backward:
+            penalty.backward(retain_graph=True)    
+
+        return penalty
+
+    def r1_regularizer(self, real, D, scaler=None):
+        '''R1 regularizer
+
+        args:
+            real: torch.Tensor
+                batched tensor of real images
+            D: torch.nn.Module
+                discriminator to calculate the gradient for
+            scaler: torch.cuda.amp.GradScaler (default: None)
+                gradient scaler for torch.cuda.amp
+        '''
+        real_loc = Variable(real, requires_grad=True)
+        d_real_loc = D(real_loc)
+
+        gradients = _calc_grad(d_real_loc, real_loc, scaler)
+
+        gradients = gradients.reshape(gradients.size(0), -1)
+        penalty = gradients.norm(2, dim=1).pow(2).mean() / 2.
+
+        if self.backward:
+            penalty.backward(retain_graph=True)
+
+        return penalty
+
+    def r2_regularizer(self, fake, D, scaler=None):
+        return self.r1_regularizer(fake, D, scaler)
