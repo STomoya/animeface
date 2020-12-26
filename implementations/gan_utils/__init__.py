f9016c3aa0acf51b1ab0feae2815040dcae19932
STomoya <blackie0110@gmail.com>
2020-12-26 23:02:35 +0900

    [FIX] import in __init__

--- implementations/gan_utils/__init__.py
+++ implementations/gan_utils/__init__.py
@@ -1,6 +1,6 @@
 # import loss functions from gan_utils.losses
 from .utils import *
-from .ema import EMA
 from .DiffAugment_pytorch import DiffAugment
 from .DiffAugment_kornia import kDiffAugment
 from .ada import AdaAugment
+from .AdaBelief import AdaBelief
