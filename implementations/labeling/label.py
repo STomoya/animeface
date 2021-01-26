8125f3ea23b2c99186600a45ef669bceb7e02e94
STomoya <blackie0110@gmail.com>
2021-01-26 22:09:50 +0900

    [UPDATE] reimplement labeling

--- implementations/labeling/label.py
+++ implementations/labeling/label.py
@@ -1,76 +1,77 @@
 
-from pathlib import Path
+import glob
+from collections import Counter
 
 import i2v
 from PIL import Image
 from tqdm import tqdm
 
-base = Path('/usr/src/data/images')
-image_paths = list(base.glob('*'))
-
-illust2vec = i2v.make_i2v_with_chainer('/usr/src/data/illustration2vec/illust2vec_tag_ver200.caffemodel', '/usr/src/data/illustration2vec/tag_list.json')
-
 '''
 Tags
-Some of the tags are eliminated, because of having too small number of samples. (Threshold : #samples > 50)
-
-Original
 ['blonde hair', 'brown hair', 'black hair', 'blue hair', 'pink hair', 'purple hair', 'green hair',
 'red hair', 'silver hair', 'white hair', 'orange hair', 'aqua hair', 'grey hair', 'long hair',
 'short hair', 'twintails', 'drill hair', 'ponytail', 'blush', 'smile', 'open mouth',
 'hat', 'ribbon', 'glasses', 'blue eyes', 'red eyes', 'brown eyes', 'green eyes',
 'purple eyes', 'yellow eyes', 'pink eyes', 'aqua eyes', 'black eyes', 'orange eyes']
-
-->
-
-Changed
-['blonde hair', 'brown hair', 'black hair', 'blue hair', 'pink hair', 'purple hair', 'green hair',
-'red hair', 'silver hair', 'white hair', 'orange hair', 'aqua hair', 'grey hair', 'long hair',
-'short hair',                            'ponytail', 'blush', 'smile', 'open mouth',
-'hat',           'glasses', 'blue eyes', 'red eyes', 'brown eyes', 'green eyes',
-'purple eyes', 'yellow eyes', 'pink eyes', 'aqua eyes', 'black eyes'               ]
 '''
 
-tags = ['blonde hair', 'brown hair', 'black hair', 'blue hair', 'pink hair', 'purple hair', 'green hair',
-        'red hair', 'silver hair', 'white hair', 'orange hair', 'aqua hair', 'grey hair', 'long hair',
-        'short hair', 'ponytail', 'blush', 'smile', 'open mouth',
-        'hat', 'glasses', 'blue eyes', 'red eyes', 'brown eyes', 'green eyes',
-        'purple eyes', 'yellow eyes', 'pink eyes', 'aqua eyes', 'black eyes']
+TAGS = ['blonde hair', 'brown hair', 'black hair', 'blue hair', 'pink hair', 'purple hair', 'green hair',
+    'red hair', 'silver hair', 'white hair', 'orange hair', 'aqua hair', 'grey hair', 'long hair',
+    'short hair', 'twintails', 'drill hair', 'ponytail', 'blush', 'smile', 'open mouth',
+    'hat', 'ribbon', 'glasses', 'blue eyes', 'red eyes', 'brown eyes', 'green eyes',
+    'purple eyes', 'yellow eyes', 'pink eyes', 'aqua eyes', 'black eyes', 'orange eyes']
 
-lines = []
+def get_model():
+    return i2v.make_i2v_with_chainer(
+        '/usr/src/data/illustration2vec/illust2vec_tag_ver200.caffemodel',
+        '/usr/src/data/illustration2vec/tag_list.json'
+    )
 
-for path in tqdm(image_paths):
-    # extract year from path (**/<id>_<year>.jpg)
-    year = path.name.split('.')[0].split('_')[-1]
+def predict_sort_top(image_path, model, tags=TAGS):
+    image = Image.open(image_path)
+    tag2prob = model.estimate_specific_tags([image], tags)
+    tag2prob = sorted(tag2prob[0].items(), key=lambda x: x[1], reverse=True)
+    return tag2prob[0]
 
-    image = Image.open(path)
-    # extract
-    tag2prob = illust2vec.estimate_specific_tags([image], tags)
+def label_it(image_paths, threshold=0.5):
 
-    # skip when no response
-    if tag2prob == []:
-        print(path, 'has been skipped')
-        continue
+    model = get_model()
 
-    # sort dict by value
-    sorted_tag2prob = sorted(tag2prob[0].items(), key=lambda x: x[1], reverse=True)
+    path2tag = {}
+    for image in tqdm(image_paths):
+        tag2prob = predict_sort_top(image, model)
+        if tag2prob[1] > threshold:
+            path2tag[image] = tag2prob[0]
+    
+    return path2tag
 
-    line = ','.join([str(path), sorted_tag2prob[0][0].replace(' ', '_'), year])    
-    lines.append(line)
+def analysis(path2tag):
+    tag_list = list(path2tag.values())
+    tag_counter = Counter(tag_list)
+    print(tag_counter)
 
-with open('/usr/src/data/animefacedataset/labels.csv', 'w', encoding='utf-8') as fout:
-    fout.write('\n'.join(lines))
+def _save(path2tag, filename: str):
+    assert filename.endswith('.csv'), 'input filename with ".csv" extension.'
 
-# analysis
-from collections import Counter
-from pprint import pprint
-print('Images : {} / {}'.format(len(lines), len(image_paths)))
-i2vtag_label_list = [line.split(',')[1] for line in lines]
-year_label_list = [line.split(',')[2] for line in lines]
+    lines = [','.join([file, tag]) for file, tag in path2tag.items()]
+    with open(filename, 'w', encoding='utf-8') as fout:
+        fout.write('\n'.join(lines))
 
-i2vtag_counter = Counter(i2vtag_label_list)
-year_counter = Counter(year_label_list)
+def animeface():
+    threshold = 0.5
+    analysis_ = True
+    save_filename = '/usr/src/data/animefacedataset/labels.csv'
 
-pprint(i2vtag_counter)
-pprint(year_counter)
+    image_paths = glob.glob('/usr/src/data/animefacedataset/images/*')
+    path2tag = label_it(image_paths, 0.5)
+    if analysis_:
+        analysis(path2tag)
+    _save(path2tag, save_filename)
+
+def danbooru():
+    threshold = 0.5
+
+if __name__=='__main__':
+    animeface()
+
 
