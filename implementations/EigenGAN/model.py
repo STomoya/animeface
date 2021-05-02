9da959d3d071390a9278d03084c7931c3ad68c79
STomoya <blackie0110@gmail.com>
2021-05-02 10:10:25 +0900

    [FIX] subspace module implement

--- implementations/EigenGAN/model.py
+++ implementations/EigenGAN/model.py
@@ -32,17 +32,23 @@ def Linear(sn, *args, **kwargs):
 class Subspace(nn.Module):
     def __init__(self, latent_dim, channels, resolution):
         super().__init__()
-        self.U = nn.Parameter(torch.empty(channels, resolution, resolution, latent_dim))
-        nn.init.orthogonal_(self.U.data)
+        self.U = nn.Parameter(torch.empty(latent_dim, channels, resolution, resolution))
+        nn.init.orthogonal_(self.U)
         l_init = [[3.*i for i in range(latent_dim, 0, -1)]]
         self.L = nn.Parameter(torch.tensor(l_init))
         self.mu = nn.Parameter(torch.zeros(1, channels, resolution, resolution))
     
     def forward(self, z):
 
-        x = (self.L * z)[:, None, None, None, :]
+        # [1N] * [BN] = [BN]
+        # [BN] -> [BN111]
+        x = (self.L * z)[:, :, None, None, None]
+        # [NCHW] -> [1NCHW]
+        # [1NCHW] * [BN111] = [BNCHW]
         x = self.U[None, ...] * x
-        x = x.sum(-1)
+        # sum over latent input dim
+        # [BNCHW] -> [BCHW]
+        x = x.sum(1)
 
         x = x + self.mu
         return x
@@ -54,7 +60,7 @@ class Subspace(nn.Module):
         basis = vector[0:1] / vector[0:1].norm()
         for i in range(1, vector.size(0)):
             v = vector[i:i+1]
-            w = torch.mm(torch.mm(v, basis.T), basis)
+            w = v - torch.mm(torch.mm(v, basis.T), basis)
             w = w / w.norm()
             basis = torch.cat([basis, w], dim=0)
         return basis
@@ -205,3 +211,16 @@ if __name__=='__main__':
     out = g(x, z)
     prob = d(out)
     print(out.size(), prob.size())
+
+    @torch.enable_grad()
+    def orthogonal_regularizer(model):
+        loss = 0
+        for name, param in model.named_parameters():
+            if 'U' in name:
+                flat = param.view(param.size(0), -1)
+                sym = torch.mm(flat, flat.T)
+                eye = torch.eye(sym.size(-1), device=sym.device)
+                loss = loss + (sym - eye).pow(2).sum() * 0.5
+        return loss
+
+    print(orthogonal_regularizer(g))
