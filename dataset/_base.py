6d471b8c3d607dc58f9d8b5226f30a5eaddef062
STomoya <blackie0110@gmail.com>
2021-07-12 20:12:17 +0900

    [ADD] dataset base

---
+++ dataset/_base.py
@@ -0,0 +1,211 @@
+
+import os
+import random
+
+import torch
+import torchvision.transforms as T
+import torchvision.transforms.functional as TF
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image as pilImage
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+def make_default_transform(
+    image_size: int,
+    resize_scale: float=1.,
+    hflip: bool=True,
+    normalize: bool=True
+):
+    '''default transforms
+    '''
+    t_list = [
+        T.Resize(int(image_size*resize_scale)),
+        T.CenterCrop(image_size)
+    ]
+    if hflip:
+        t_list.append(T.RandomHorizontalFlip())
+
+    t_list.append(T.ToTensor())
+    if normalize:
+        t_list.append(T.Normalize(0.5, 0.5))
+
+    return T.Compose(t_list)
+
+class WrappedDataset(Dataset):
+    '''Wrapped Dataset class
+    with some methods'''
+    def _load(self):
+        raise NotImplementedError()
+
+    @classmethod
+    def asloader(cls,
+        batch_size: int,
+        cls_args: tuple,
+        cls_kwargs: dict={},
+        shuffle: bool=True,
+        num_workers: int=os.cpu_count(),
+        pin_memory: bool=torch.cuda.is_available()
+    ):
+        dataset = cls(*cls_args, **cls_kwargs)
+        return DataLoader(
+            dataset, batch_size, shuffle,
+            num_workers=num_workers, pin_memory=pin_memory)
+
+class Image(WrappedDataset):
+    '''Image dataset base class
+    '''
+    def __init__(self, transform):
+        self.images = self._load()
+        self.transform = transform
+
+    def __getitem__(self, index):
+        image = self.images[index]
+
+        image = pilImage.open(image).convert('RGB')
+        image = self.transform(image)
+
+        return image
+
+    def __len__(self):
+        return len(self.images)
+
+class ImageImage(WrappedDataset):
+    '''Image Image dataset base class
+    '''
+    def __init__(self, transform):
+        self.images1, self.images2 = self._load()
+        self.transform = transform
+
+    def __getitem__(self, index):
+        image1 = self.images1[index]
+        image2 = self.images2[index]
+
+        image1 = pilImage.open(image1).convert('RGB')
+        image2 = pilImage.open(image2).convert('RGB')
+        image1 = self.transform(image1)
+        image2 = self.transform(image2)
+
+        return image1, image2
+
+    def __len__(self):
+        return len(self.images1)
+
+class LRHR(WrappedDataset):
+    '''Low Resolution, High Resolution dataset base class
+    '''
+    def __init__(self, image_size, scale=2, resize_ratio=1.):
+        self.images = self._load()
+        self.image_size = image_size
+        self.resize_ratio = resize_ratio
+        self.scale = scale
+
+    def __getitem__(self, index):
+        image = self.images[index]
+
+        image = pilImage.open(image).convert('RGB')
+        lr, sr = self.transform(image)
+
+        return lr, sr
+
+    def transform(self, img):
+        return self._default_transform(img)
+
+    def _default_transform(self, img):
+        lr_size = self.image_size // self.scale
+        sr = TF.resize(img, int(self.image_size*self.resize_ratio))
+        sr = TF.center_crop(sr, self.image_size)
+        lr = TF.resize(img, int(lr_size*self.resize_ratio))
+        lr = TF.center_crop(lr, lr_size)
+
+        if random.random() > 0.5:
+            sr = TF.hflip(sr)
+            lr = TF.hflip(lr)
+
+        sr = TF.to_tensor(sr)
+        sr = TF.normalize(sr, 0.5, 0.5)
+        lr = TF.to_tensor(lr)
+        lr = TF.normalize(lr, 0.5, 0.5)
+        return lr, sr
+
+    def __len__(self):
+        return len(self.images)
+
+class ImageXDoG(WrappedDataset):
+    '''Image + XDoG dataset base class
+    '''
+    def __init__(self, transform):
+        self.images, self.xdogs = self._load()
+        self.transform = transform
+
+    def __getitem__(self, index):
+        image = self.images[index]
+        xdog  = self.xdogs[index]
+
+        image = pilImage.open(image).convert('RGB')
+        xdog  = pilImage.open(xdog).convert('L')
+
+        image = self.transform(image)
+        xdog  = self.transform(xdog)
+
+        return image, xdog
+
+    def shuffle_xdog(self):
+        random.shuffle(self.xdogs)
+
+    def __len__(self):
+        return len(self.images)
+
+class ImageLabel(WrappedDataset):
+    '''Image + Label dataset base class
+    '''
+    def __init__(self, transform):
+        self.images, labels = self._load()
+        self.transform = transform
+
+        self._make_label(labels)
+
+    def _make_label(self, labels):
+        self.encoder = LabelEncoder()
+        labels = np.array(labels).reshape(-1)
+        self.labels = self.encoder.fit_transform(labels)
+        self.num_classes = len(self.encoder.classes_)
+
+    def __getitem__(self, index):
+        image = self.images[index]
+        label = self.labels[index]
+
+        image = pilImage.open(image).convert('RGB')
+        image = self.transform(image)
+
+        return image, label
+
+    def inverse_transform(self, label):
+        if isinstance(label, torch.Tensor):
+            label = label.cpu().numpy()
+        if not isinstance(label, np.ndarray):
+            label = np.array(label)
+        label = label.reshape(-1)
+        return self.encoder.inverse_transform(label)
+
+    def __len__(self):
+        return len(self.images)
+
+class ImageOnehot(ImageLabel):
+    '''Image + One-Hot label dataset base class
+    '''
+    def __init__(self, transform):
+        super().__init__(transform)
+
+    def _make_label(self, labels):
+        self.encoder = OneHotEncoder()
+        labels = np.array(labels).reshape(-1, 1)
+        self.labels = self.encoder.fit_transform(labels).toarray()
+        self.num_classes = len(self.encoder.categories_[0])
+
+    def inverse_transform(self, label):
+        if isinstance(label, torch.Tensor):
+            label = label.cpu().numpy()
+        if not isinstance(label, np.ndarray):
+            label = np.array(label)
+        label = label.reshape(1, -1)
+        return self.encoder.inverse_transform(label)
