9375ba3e67f72f21af244886eb0cb15e67800548
STomoya <blackie0110@gmail.com>
2021-07-13 12:48:26 +0900

    [UP] use new utils, support debug

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 
+import sys
+
 from importlib import import_module
-from parser import get_common_parser
+from utils import argument, debug_mode
+from loguru import logger
+
+logger.remove()
+logger.add(sys.stdout, level='WARNING')
 
 def main():
-    parser = get_common_parser()
-    args, _ = parser.parse_known_args()
+    parser = argument.get_default_parser()
+    args = parser.parse_known_args()[0]
+    if args.debug:
+        debug_mode()
+        logger.info(args)
     module = import_module(f'.{args.name}', 'implementations')
     module.main(parser)
-
 
 if __name__ == "__main__":
     main()
