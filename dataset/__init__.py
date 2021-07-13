1c531ce02a1f12dbfc23461d59a6327badaee359
STomoya <blackie0110@gmail.com>
2021-07-13 18:13:12 +0900

    [UP] inf sampler, to_loader

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -20,3 +20,26 @@ from dataset.danbooru import (
     DanbooruSR,
     DanbooruAutoPair
 )
+
+import os
+import torch
+from torch.utils.data import DataLoader, Dataset
+from collections.abc import Iterable
+
+def cycle(iterable: Iterable):
+    while True:
+        for i in iterable:
+            yield i
+
+def to_loader(
+    dataset: Dataset,
+    batch_size: int,
+    shuffle: bool=True,
+    num_workers: int=os.cpu_count(),
+    pin_memory: bool=torch.cuda.is_available()
+) -> DataLoader:
+    loader = DataLoader(
+        dataset, batch_size, shuffle=shuffle,
+        num_workers=num_workers, pin_memory=pin_memory
+    )
+    return loader
