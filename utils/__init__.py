5360c0cb8e36aca911bbfff2b9f41bb2e545bf9e
STomoya <blackie0110@gmail.com>
2021-07-13 18:30:17 +0900

    [UP] gif saver

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 from typing import Any
+from PIL import Image
 
 def debug_mode():
     from utils import debug
@@ -21,3 +22,19 @@ class EasyDict(dict):
 
     def __delattr__(self, name: str) -> None:
         del self[name]
+
+def gif_from_files(
+    image_paths: list[str],
+    filename: str='out.gif',
+    optimize: bool=False,
+    duration: int=500,
+    loop: int=0
+) -> None:
+    images = [Image.open(str(path)) for path in image_paths]
+    images[0].save(filename,
+        save_all=True,
+        append_images=images[1:],
+        optimize=optimize,
+        duration=duration,
+        loop=loop
+    )
