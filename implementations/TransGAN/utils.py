df8a1899964bb105dc942c553af4dec2d82a92f6
STomoya <blackie0110@gmail.com>
2021-09-17 16:53:55 +0900

    [UP] refacter transgan

--- implementations/TransGAN/utils.py
+++ implementations/TransGAN/utils.py
@@ -7,31 +7,27 @@ import torch.optim as optim
 from torch.cuda.amp import GradScaler, autocast
 from torchvision.utils import save_image
 
-from ..general import YearAnimeFaceDataset, DanbooruPortraitDataset, to_loader
-from ..general import save_args, get_device, Status
-from ..gan_utils import sample_nnoise, DiffAugment, update_ema
-from ..gan_utils.losses import HingeLoss, WGANLoss, GANLoss, GradPenalty
+from dataset import AnimeFace, DanbooruPortrait
+from utils import Status, save_args, add_args
+from nnutils import sample_nnoise, update_ema, get_device
+from nnutils.loss import NonSaturatingLoss, r1_regularizer
+from thirdparty.diffaugment import DiffAugment
 
 from .model import Generator, Discriminator, init_weight_normal
 
 def train(
     max_iters, dataset, latent_dim, sampler, const_z,
     G, G_ema, D, optimizer_G, optimizer_D,
-    loss_type, policy, gp_lambda, ema_decay,
+    policy, gp_lambda, ema_decay,
     device, amp, save=1000
 ):
-    
+
     status = Status(max_iters)
     scaler = GradScaler() if amp else None
 
-    if loss_type == 'hinge':
-        loss = HingeLoss()
-    elif loss_type == 'wgan_gp':
-        loss = WGANLoss()
-        gp = GradPenalty().gradient_penalty
-    elif loss_type == 'gan':
-        loss = GANLoss()
-        gp = GradPenalty().r1_regularizer
+    loss = NonSaturatingLoss()
+    gp = r1_regularizer()
+
     if policy is not '':
         augment = functools.partial(DiffAugment, policy=policy)
     else:
@@ -56,11 +52,8 @@ def train(
                 fake_prob = D(fake_aug.detach())
                 # loss
                 D_loss = loss.d_loss(real_prob, fake_prob)
-                if loss_type == 'wgan_gp':
-                    D_loss = D_loss + gp(real, fake, D, scaler, center=1.) * gp_lambda
-                if loss_type == 'gan':
-                    D_loss = D_loss + gp(real, D, scaler) * gp_lambda
-            
+                D_loss = D_loss + gp(real, D, scaler) * gp_lambda
+
             if scaler is not None:
                 scaler.scale(D_loss).backward()
                 scaler.step(optimizer_D)
@@ -74,14 +67,14 @@ def train(
                 fake_prob = D(fake_aug)
                 # loss
                 G_loss = loss.g_loss(fake_prob)
-            
+
             if scaler is not None:
                 scaler.scale(G_loss).backward()
                 scaler.step(optimizer_G)
             else:
                 G_loss.backward()
                 optimizer_G.step()
-            
+
             if G_ema is not None:
                 update_ema(G, G_ema, ema_decay)
 
@@ -107,57 +100,49 @@ def train(
                 G=G_loss.item() if not torch.isnan(G_loss).any() else 0.,
                 D=D_loss.item() if not torch.isnan(D_loss).any() else 0.
             )
-            status.update(loss_dict)
+            status.update(**loss_dict)
             if scaler is not None:
                 scaler.update()
 
             if status.batches_done == max_iters:
                 break
 
-
-def add_arguments(parser):
-
-    # model
-    parser.add_argument('--image-channels', default=3, type=int, help='channels of the output image')
-    # G
-    parser.add_argument('--latent-dim', default=128, type=int, help='dimension of latent input')
-    parser.add_argument('--g-depths', default=[5, 4, 2], type=int, nargs='+', help='number of transformer blocks in each reslution')
-    parser.add_argument('--bottom-width', default=8, type=int, help='first resolution')
-    parser.add_argument('--g-embed-dim', default=1024, type=int, help='dimension of embedding in G. times of 4')
-    parser.add_argument('--g-num-heads', default=4, type=int, help='number of heads in multi-head attention in G')
-    parser.add_argument('--g-mlp-ratio', default=4, type=int, help='ratio for dimension of features of hidden layers in mlp in G')
-    parser.add_argument('--g-use-qkv-bias', default=False, action='store_true', help='use bias for query, key and value in attention in G')
-    parser.add_argument('--g-dropout', default=0.1, type=float, help='dropout probability in G')
-    parser.add_argument('--g-attn-dropout', default=0.1, type=float, help='dropout probability for heatmap in attetion in G')
-    parser.add_argument('--g-act-name', default='gelu', choices=['gelu'], help='activation function in G')
-    parser.add_argument('--g-norm-name', default='ln', choices=['ln'], help='normalization layer name in G')
-    # D
-    parser.add_argument('--patch-size', default=8, type=int, help='size of each patch')
-    parser.add_argument('--d-depth', default=7, type=int, help='number of encoders in D')
-    parser.add_argument('--d-embed-dim', default=384, type=int, help='dimension of embedding in D. times of 4')
-    parser.add_argument('--d-num-heads', default=4, type=int, help='number of heads in multi-head attention in D')
-    parser.add_argument('--d-mlp-ratio', default=4, type=int, help='ratio for dimension of features of hidden layers in mlp in G')
-    parser.add_argument('--d-use-qkv-bias', default=False, action='store_true', help='use bias for query, key and value in attention in D')
-    parser.add_argument('--d-dropout', default=0.1, type=float, help='dropout probability in D')
-    parser.add_argument('--d-attn-dropout', default=0.1, type=float, help='dropout probability for heatmap in attetion in D')
-    parser.add_argument('--d-act-name', default='gelu', choices=['gelu'], help='activation function in D')
-    parser.add_argument('--d-norm-name', default='ln', choices=['ln'], help='normalization layer name in D')
-
-    # training
-    parser.add_argument('--lr', default=0.00001, type=float, help='learning rate')
-    parser.add_argument('--ttur', default=False, action='store_true', help='use TTUR')
-    parser.add_argument('--beta1', default=0.5, type=float, help='beta1')
-    parser.add_argument('--beta2', default=0.999, type=float, help='beta2')
-    parser.add_argument('--loss-type', default='gan', choices=['hinge', 'wgan_gp', 'gan'], help='loss type')
-    parser.add_argument('--gp-lambda', default=10., type=float, help='lambda for gradient penalty')
-    parser.add_argument('--policy', default='color,translation', type=str, help='policy for DiffAugment')
-    parser.add_argument('--ema', default=False, action='store_true', help='exponential moving average')
-    parser.add_argument('--ema-decay', default=0.999, type=float, help='decay for EMA')
-    return parser
+    status.plot_loss()
 
 def main(parser):
 
-    parser = add_arguments(parser)
+    parser = add_args(parser,
+        dict(
+            image_channels = [3, 'channels of the output image'],
+            latent_dim     = [128, 'dimension of latent input'],
+            g_depths       = [[5, 4, 2], 'number of transformer blocks in each reslution'],
+            bottom_width   = [8, 'first resolution'],
+            g_embed_dim    = [1024, 'dimension of embedding in G. times of 4'],
+            g_num_heads    = [4, 'number of heads in multi-head attention in G'],
+            g_mlp_ratio    = [4, 'ratio for dimension of features of hidden layers in mlp in G'],
+            g_use_qkv_bias = [False, 'use bias for query, key and value in attention in G'],
+            g_dropout      = [0.1, 'dropout probability in G'],
+            g_attn_dropout = [0.1, 'dropout probability for heatmap in attetion in G'],
+            g_act_name     = ['gelu', 'activation function in G'],
+            g_norm_name    = ['ln', 'normalization layer name in G'],
+            patch_size     = [8, 'size of each patch'],
+            d_depth        = [7, 'number of encoders in D'],
+            d_embed_dim    = [384, 'dimension of embedding in D. times of 4'],
+            d_num_heads    = [4, 'number of heads in multi-head attention in D'],
+            d_mlp_ratio    = [4, 'ratio for dimension of features of hidden layers in mlp in G'],
+            d_use_qkv_bias = [False, 'use bias for query, key and value in attention in D'],
+            d_dropout      = [0.1, 'dropout probability in D'],
+            d_attn_dropout = [0.1, 'dropout probability for heatmap in attetion in D'],
+            d_act_name     = ['gelu', 'activation function in D'],
+            d_norm_name    = ['ln', 'normalization layer name in D'],
+            lr             = [0.00001, 'learning rate'],
+            ttur           = [False, 'use TTUR'],
+            beta1          = [0.5, 'beta1'],
+            beta2          = [0.999, 'beta2'],
+            gp_lambda      = [10., 'lambda for gradient penalty'],
+            policy         = ['color,translation', 'policy for DiffAugment'],
+            ema            = [False, 'exponential moving average'],
+            ema_decay      = [0.999, 'decay for EMA']))
     args = parser.parse_args()
     save_args(args)
 
@@ -166,10 +151,13 @@ def main(parser):
 
     # dataset
     if args.dataset == 'animeface':
-        dataset = YearAnimeFaceDataset(args.image_size, args.min_year)
+        dataset = AnimeFace.asloader(
+            args.batch_size, (args.image_size, args.min_year),
+            pin_memory=not args.disable_gpu)
     elif args.dataset == 'danbooru':
-        dataset = DanbooruPortraitDataset(args.image_size, num_images=args.num_images)
-    dataset = to_loader(dataset, args.batch_size)
+        dataset = DanbooruPortrait.asloader(
+            args.batch_size, (args.image_size, args.num_images),
+            pin_memory=not args.disable_gpu)
 
     if args.max_iters < 0:
         args.max_iters = len(dataset) * args.default_epochs
@@ -181,7 +169,7 @@ def main(parser):
     assert expected_g_depths_len == len(args.g_depths), \
           f'Expected number of args for "--g-depths" for your setting is {expected_g_depths_len}. ' \
         + f'You have {len(args.g_depths)}.'
-    
+
     G = Generator(
         args.g_depths, args.latent_dim, args.image_channels,
         args.bottom_width, args.g_embed_dim, args.g_num_heads,
@@ -222,6 +210,6 @@ def main(parser):
     train(
         args.max_iters, dataset, args.latent_dim, sampler, const_z,
         G, G_ema, D, optimizer_G, optimizer_D,
-        args.loss_type, args.policy, args.gp_lambda, args.ema_decay,
+        args.policy, args.gp_lambda, args.ema_decay,
         device, amp, args.save
     )
