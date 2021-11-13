8836223dcbbdcbeec98bbc0d31c394cf7ea0f70b
STomoya <stomoya0110@gmail.com>
2021-11-13 13:27:01 +0900

    [UP] better logging

--- utils/status.py
+++ utils/status.py
@@ -1,7 +1,14 @@
 '''utils for training status'''
 
+import logging
+import time, datetime
+import subprocess
+import pprint
 import os
 import warnings
+
+import torch
+from torch.utils.collect_env import get_pretty_env_info
 from tqdm import tqdm
 
 class Status:
@@ -19,17 +26,33 @@ class Status:
         max_iters: int,
         bar: bool=True,
         log_file: str=None,
-        log_interval: int=1
+        log_interval: int=1,
+        logger_name: str='logger'
     ) -> None:
         if bar:
             self.bar = tqdm(total=max_iters)
-        self.max_iters = max_iters
-        self.batches_done = 0
-        self.loss = None
-        self.log_file = log_file
-        if isinstance(log_file, str) and os.path.exists(log_file):
-            self._write_to_log('', 'w')
-        self.log_interval = log_interval
+        self._max_iters = max_iters
+        self._batches_done = 0
+        self._loss = None
+        self._log_file = log_file
+        if log_file is not None:
+            logging.basicConfig(
+                filename=log_file, filemode='w',
+                format='%(asctime)s:%(filename)s:%(levelname)s: %(message)s')
+            self._logger = logging.getLogger(logger_name)
+            self._logger.setLevel(logging.DEBUG)
+        self._log_interval = log_interval
+        self._step_start = time.time()
+
+    @property
+    def max_iters(self):
+        return self._max_iters
+    @property
+    def batches_done(self):
+        return self._batches_done
+    @batches_done.setter
+    def batches_done(self, value):
+        self._batches_done = value
 
     def print(self, *args, **kwargs):
         '''print function'''
@@ -37,33 +60,70 @@ class Status:
             tqdm.write(*args, **kwargs)
         else:
             print(*args, **kwargs)
+    def log(self, message, level='info'):
+        if hasattr(self, '_logger'):
+            getattr(self._logger, level)(message)
+        else:
+            warnings.warn('No Logger. Printing to stdout.')
+            self.print(message)
 
-    def _write_to_log(self, content: str, mode='a'):
-        '''write to log file if specified'''
-        if self.log_file is None:
-            return
-        if not content.endswith('\n'):
-            content += '\n'
-        with open(self.log_file, mode) as fout:
-            fout.write(content)
-    def log(self, content):
-        '''wrapper for _write_to_log'''
-        self._write_to_log(content)
+    '''Information loggers'''
+
+    def log_args(self, args):
+        dict_args = pprint.pformat(vars(args))
+        self.log(f'Command line arguments\n{dict_args}')
+
+    def log_torch(self):
+        env = get_pretty_env_info()
+        self.log(f'PyTorch environment:\n{env}')
+
+    def log_models(self, *models):
+        for model in models:
+            self.log(f'Architecture: {model.__class__.__name__}\n{model}')
+
+    def log_gpu(self):
+        if torch.cuda.is_available():
+            nvidia_smi_output = subprocess.run(
+                'nvidia-smi', shell=True,
+                capture_output=True, universal_newlines=True)
+            self.log(f'\n{nvidia_smi_output.stdout}')
+        else:
+            self.log('No GPU available on your enviornment.')
+
+    def log_training(self, args, *models):
+        '''log information in one function'''
+        self.log_args(args)
+        self.log_torch()
+        self.log_models(*models)
+
+    '''a step'''
 
     def update(self, **kwargs) -> None:
         '''update status'''
-        if self.loss == None:
+        if self._loss == None:
             self._init_loss(kwargs.keys())
 
         postfix = []
         for k, v in kwargs.items():
-            self.loss[k].append(v)
+            self._loss[k].append(v)
             postfix.append(f'{k} : {v:.5f}')
 
-        if self.batches_done % self.log_interval == 0:
-            self._write_to_log(self.__str__())
+        # log
+        if self._log_file is not None \
+            and self.batches_done % self._log_interval == 0:
+            # FIXME: this ETA is not exact.
+            duration = time.time() - self._step_start
+            eta_sec = int((self.max_iters - self.batches_done) * duration)
+            eta = datetime.timedelta(seconds=eta_sec)
+            self.log(
+                f'STEP: {self.batches_done} / {self.max_iters} INFO: {kwargs} ETA: {eta}')
+        if self.batches_done == 0:
+            # print gpu on first step
+            # for checking memory usage
+            self.log_gpu()
 
         self.batches_done += 1
+        self._step_start = time.time()
 
         if hasattr(self, 'bar'):
             self.bar.set_postfix_str(' '.join(postfix))
@@ -75,7 +135,7 @@ class Status:
     def load_state_dict(self, state_dict: dict) -> None:
         '''fast forward'''
         # load
-        self.loss = state_dict['loss']
+        self._loss = state_dict['loss']
         self.batches_done = state_dict['batches_done']
         if self.batches_done > 0:
             # fastforward progress bar if present
@@ -84,14 +144,14 @@ class Status:
 
     def state_dict(self) -> dict:
         return dict(
-            loss=self.loss,
+            loss=self._loss,
             batches_done=self.batches_done)
 
     def _init_loss(self, keys):
         '''initialize loss'''
-        self.loss = {}
+        self._loss = {}
         for key in keys:
-            self.loss[key] = []
+            self._loss[key] = []
 
     def plot_loss(self, filename='loss'):
         '''plot loss'''
@@ -105,7 +165,7 @@ class Status:
 
         plt.figure(figsize=(12, 8))
         legends = []
-        for key, values in self.loss.items():
+        for key, values in self._loss.items():
             legends.append(key)
             plt.plot(values)
         plt.xlabel('iteration')
@@ -117,10 +177,10 @@ class Status:
 
     def __str__(self):
         '''return latest loss by string'''
-        if self.loss == None:
+        if self._loss == None:
             return 'Loss untracked.'
 
         string = [f'Batch : {self.batches_done}\t']
-        for k, v in self.loss.items():
+        for k, v in self._loss.items():
             string.append(f'{k} : {v[-1]:.5f}')
         return ' '.join(string)
